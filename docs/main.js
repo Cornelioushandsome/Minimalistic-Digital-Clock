3135adcdfb8015f8d980ce2b6d7fe91fe921dd6b
Cornelioushandsome <145301075+Cornelioushandsome@users.noreply.github.com>
2025-06-13 16:40:30 -0400

    Add files via upload

--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,42 @@
 const timezoneSelecter = document.getElementById("timezone");
 const clockTime=document.getElementById("clock");
 const dayTime = document.getElementById("day");
+const addTaskButton = document.getElementById("add-task-button");
+const taskForm = document.getElementById("task-form");
+const taskSubmitButton = document.getElementById("taskSubmit");
+const taskName = document.getElementById("taskName");
 
 
-const weekdays = ["Saturday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August",
      "September", "October", "November", "December"];
+
+
+addTaskButton.addEventListener("click", () =>{
+    taskForm.classList.toggle("hidden");
+});
+
+function toTwelveHour(time){
+    let timeArray = time.split(":");
+    let hours = (parseInt(timeArray[0], 10) % 12 || 12).toString();
+    let minutes = (parseInt(timeArray[1], 10)).toString();
+    hours = hours.padStart(2,0);
+    minutes = minutes.padStart(2,0);
+    const meridian = parseInt(timeArray[0], 10)>= 12 ? "PM" : "AM";
+    return `${hours}:${minutes} ${meridian}`;
+}
+
+taskSubmitButton.addEventListener("click", () => {
+    let start = document.getElementById("startTime").value;
+    let end = document.getElementById("endTime").value;
+    const name = document.getElementById("taskName");
+    if(!start || !end || !name.value.trim()){
+        window.alert("Please fill out all of the fields");
+        return;
+    }
+    console.log(`"${name.value}" task from ${toTwelveHour(start)} to ${toTwelveHour(end)}`);
+    taskForm.classList.add("hidden");
+});
 
 function getTimezone(){
     const timezone=new Date().toLocaleTimeString("en-us", {timeZoneName: "short"}).split(" ")[2];
@@ -33,8 +64,9 @@ function updateClock(){
     dayTime.textContent=dayString;
 }
 
-
 getTimezone();
 updateClock();
 setInterval(updateClock, 1000);
+
+
 
